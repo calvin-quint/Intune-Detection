71eb5caca04eab5fb801da95fa634c0ae281c5d4
calvin-quint <138726054+calvin-quint@users.noreply.github.com>
2024-03-27 15:23:14 -0500

    Update Intune-Detection-03-26.ps1

--- Intune-Detection-03-26.ps1
+++ Intune-Detection-03-26.ps1
@@ -7,6 +7,8 @@ $registryPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
 # Query the registry for information about 7-Zip
 $7Zip = Get-ItemProperty -Path $registryPath | Where-Object { $_.DisplayName -like '*7-Zip*' }
 
+$exitCode = 0
+
 # If 7-Zip is found
 if ($7Zip) {
     # Retrieve the version of the installed 7-Zip
@@ -29,21 +31,16 @@ if ($7Zip) {
         # Check if uninstallation was successful
         if ($process.ExitCode -eq 0) {
             Write-Output "7-Zip version $Version has been uninstalled."
-            exit 0  # Exit with success code
-            $process.ExitCode = 0
         } else {
             Write-Output "Failed to uninstall 7-Zip. Exit code: $($process.ExitCode)"
-            exit 1  # Exit with failure code
-            $process.ExitCode = 1
+            $exitCode = 1
         }
     } else {
         Write-Output "No action required. 7-Zip version meets minimum requirement."
-        exit 0  # Exit with success code
-        $process.ExitCode = 0
     }
 } else {
     # If 7-Zip is not found
     Write-Output "7-Zip is not installed."
-    exit 1  # Exit with failure code
-    $process.ExitCode = 1
+    $exitCode = 1
 }
+exit $exitCode
